f03e9369ed5c7f86c3d6b1e8048b0c5aebeffed8
medaminefracso <medaminefracso@gmail.com>
2022-12-02 10:31:05 +0100

    added the following
    - create & delete & update a serie
    
    - added lombok getters & setters for all entities

--- src/main/java/com/technowebtp/webapp/controllers/SerieController.java
+++ src/main/java/com/technowebtp/webapp/controllers/SerieController.java
@@ -1,19 +1,64 @@
 package com.technowebtp.webapp.controllers;
 
 import com.technowebtp.webapp.models.Event;
+import com.technowebtp.webapp.models.Serie;
+import com.technowebtp.webapp.repositories.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class SerieController {
 
-    @PostMapping(value = "/series")
-    public @ResponseBody Event createSerie(@PathVariable Integer serieId) {
-        Event event = new Event();
-        event.setCommentaire("This is a comment on this event");
-        event.setValeur(7305);
+    @Autowired
+    private SerieRepository serieRepository;
 
-        return event;
+    @PostMapping(path = "/series")
+    @ResponseBody
+    public ResponseEntity createSerie(@RequestBody Serie serie) {
+
+        serieRepository.save(serie);
+
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/series/{serieId}")
+    @ResponseBody
+    public ResponseEntity deleteSerie(@PathVariable Long serieId) {
+
+        Optional<Serie> serie = serieRepository.findById(serieId);
+
+        serieRepository.delete(serie.get());
+
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+
+    @PatchMapping(path = "/series/{serieId}")
+    @ResponseBody
+    public ResponseEntity updateSerie(@PathVariable Long serieId, @RequestParam Map<String,String> allParams) {
+
+        String title, description;
+
+        Optional<Serie> serie = serieRepository.findById(serieId);
+
+        if(allParams.containsKey("title")) {
+            title = allParams.get("title");
+            serie.get().setTitle(title);
+
+        } else if(allParams.containsKey("description")) {
+            description = allParams.get("description");
+            serie.get().setDescription(description);
+        }
+
+        serieRepository.save(serie.get());
+
+        return ResponseEntity.ok(HttpStatus.OK);
     }
 
     @GetMapping(value = "/series")
@@ -34,7 +79,7 @@ public class SerieController {
         return event;
     }
 
-
+/*
     @PostMapping(value = "/series/{eventId}")
     public @ResponseBody Event updateSerie(@PathVariable Integer serieId) {
         Event event = new Event();
@@ -44,12 +89,7 @@ public class SerieController {
         return event;
     }
 
-    @DeleteMapping(value = "/series/{eventId}")
-    public @ResponseBody Event deleteSerie(@PathVariable Integer serieId) {
-        Event event = new Event();
-        event.setCommentaire("This is a comment on this event");
-        event.setValeur(7305);
+ */
 
-        return event;
-    }
+
 }

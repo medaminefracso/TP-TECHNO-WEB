ea68c10e5a394ed8c0e9532cb92ece5f984b1233
medaminefracso <medaminefracso@gmail.com>
2022-12-02 11:57:17 +0100

    created serie repository

---
+++ src/main/java/com/technowebtp/webapp/repositories/SerieRepository.java
@@ -0,0 +1,9 @@
+package com.technowebtp.webapp.repositories;
+
+
+import com.technowebtp.webapp.models.Serie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SerieRepository extends JpaRepository<Serie, Long> {
+
+}
